39dde324356ede03eb67585c8ae8151fa5be2dca
raymond.hugo <44294653+raymond-h-dev@users.noreply.github.com>
2023-10-10 11:45:53 +0800

    [#259][175887]-Update moment js library, Improper input validation and Unrestricted Field Length ASA findings (#268)
    * [#259][175887]-Update moment js library
    
    * [ASA]-Security Fixes for Form Search Inputs
    
    * fix linting issues
    
    ---------
    
    Co-authored-by: raymond.f.hugo <raymond.f.hugo@accenture.com>

--- blocks/newslist/newslist.js
+++ blocks/newslist/newslist.js
@@ -350,8 +350,9 @@ function ValidateSearchFormData(keyword) {
   if (typeof keyword === 'undefined' || keyword === null) {
     return '';
   }
-  const sanitezedKeyword = keyword.replace(/[`%$^*()_+=[\]{}\\|<>/~]/g, '');
-  return sanitezedKeyword.trim();
+  let sanitizedKeyword = keyword.replace(/[`%$^*()_+=[\]{}\\|<>/~!@#&]/g, '');
+  sanitizedKeyword = sanitizedKeyword.slice(0, 60);
+  return sanitizedKeyword.trim();
 }
 
 export default async function decorate(block) {
@@ -392,14 +393,14 @@ export default async function decorate(block) {
     searchHeader.classList.add('search-header-container');
     const form = `
       <form action="/search" method="get" id="search-form">
-        <input type="text" id="search-input" title="${pKeywords}" placeholder="${pKeywords}" name="q" value="${query}" size="40" maxlength="60">
+        <input type="text" id="search-input" title="${pKeywords}" placeholder="${pKeywords}" name="q" value="" size="40" maxlength="60">
         <input type="submit" title="${pSearch}" value="${pSearch}">
       </form>
     `;
     if (query) {
       searchHeader.innerHTML = `
       ${form}
-      <h2>Results for "${query}"</h2>
+      <h2></h2>
       <div class="search-sub-header">
         <h3> ${shortIndex.length > 0 ? 'ALL RESULTS' : '0 RESULTS WERE FOUND'} </h3>
         <div class="search-sub-header-right">
@@ -407,6 +408,10 @@ export default async function decorate(block) {
         </div>
       </div>
       `;
+      const heading = searchHeader.querySelector('h2');
+      const searchInput = searchHeader.querySelector('#search-input');
+      heading.textContent = `Results for "${query}"`;
+      searchInput.setAttribute('value', query);
     } else if (usp.get('q') === null) {
       searchHeader.innerHTML = form;
     } else {

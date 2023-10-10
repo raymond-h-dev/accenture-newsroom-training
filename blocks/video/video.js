b8e1a235c40f91c3ce0e8cf02e3447f410201d1b
Ravi Kiran Papani <ravikiran.papani@gmail.com>
2023-10-09 20:02:29 -0700

    fix: using vidyard api to load the video player rather than iframe (#289)

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,30 +1,35 @@
-import { readBlockConfig } from '../../scripts/lib-franklin.js';
+import { loadScript, readBlockConfig } from '../../scripts/lib-franklin.js';
 
-export default function decorate(block) {
+const VIDYARD_URL = 'https://play.vidyard.com/';
+const VIDYARD_SCRIPT_URL = 'https://play.vidyard.com/embed/v4.js';
+
+function getUUID(url) {
+  const { pathname } = new URL(url);
+  return pathname.substring(1);
+}
+
+export default async function decorate(block) {
   const config = readBlockConfig(block);
-  if (config.url) {
+  if (config.url || config.uuid) {
+    await loadScript(VIDYARD_SCRIPT_URL, { async: 'false' });
     const observer = new IntersectionObserver((entries) => {
       if (entries.some((e) => e.isIntersecting)) {
-        const html = `
-          <div class="video-container">
-            <div>
-                <iframe loading="lazy" allow="encrypted-media" allowfullscreen="" src="${config.url}"></iframe>
-            </div>
-          </div>
-        `;
-        block.innerHTML = html;
+        if (window.vidyardEmbed) {
+          window.vidyardEmbed.api.renderDOMPlayers();
+        }
         observer.disconnect();
       }
     });
     observer.observe(block);
     block.textContent = '';
-    const previewImage = config.preview;
-    if (previewImage) {
-      const img = document.createElement('img');
-      img.src = previewImage;
-      img.alt = config.title || '';
-      block.appendChild(img);
-    }
+    const uuid = config.uuid || getUUID(config.url);
+    const img = document.createElement('img');
+    img.classList.add('vidyard-player-embed');
+    img.src = `${VIDYARD_URL}${uuid}.jpg`;
+    img.setAttribute('data-uuid', uuid);
+    img.setAttribute('data-v', 4);
+    img.setAttribute('data-type', 'inline');
+    block.appendChild(img);
   } else {
     block.remove();
   }
